a68e05e76e0a52128b310a56640458f566339ea6
Emil Wallraff <emilwallraff@gmail.com>
2025-04-22 14:55:23 +0200

    first frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/login">Login</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,15 @@
+function Login() {
+    return (
+      <div>
+        <h2>Login</h2>
+        <form>
+          <input type="text" placeholder="Username" /><br />
+          <input type="password" placeholder="Password" /><br />
+          <button>Log In</button>
+        </form>
+      </div>
+    );
+  }
+  
+  export default Login;
+  
