40864691395526432766a8b1cf3b9e7b4975dbc3
Emil Wallraff <emilwallraff@gmail.com>
2025-08-21 21:45:26 +0200

    fixed another dictionary error

--- backend/server.py
+++ backend/server.py
@@ -269,9 +269,10 @@ async def oauth_callback(response: Response, request: Request, db: Session = Dep
         headers={"Authorization": f"Bearer {tokens['access_token']}"},
     ).json()
 
+    print(userinfo)
+
     user_id = userinfo["sub"]
-    #puuid = userinfo.get("puuid")
-    puuid = userinfo
+    puuid = userinfo.get("puuid")
     game_name = userinfo.get("acct", {}).get("game_name")
     tag_line = userinfo.get("acct", {}).get("tag_line")
 
