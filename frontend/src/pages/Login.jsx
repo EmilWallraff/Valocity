c35b02df09e170642b1fbfba9193451fb848b76b
Emil Wallraff <emilwallraff@gmail.com>
2025-08-22 11:24:06 +0200

    login frontend display experiments

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -26,27 +26,43 @@ function Login() {
       .catch(() => setUser(null));
 
     console.log("User: ", user);
+    console.log("User id []: ", user["user_id"]);
+    console.log("User id .: ", user.user_id);
   };
 
   return (
     <div className="bg-darkness items-center pt-16 p-6 space-y-16">
       <div className="flex flex-col items-center space-y-8">
+
+        <h2 className="text-4xl font-bold text-white mb-4">Profiles are still work in progress.</h2>
         <button
           onClick={handleLogin}
-          className="px-6 py-3 bg-red-600 text-white rounded-2xl shadow-md"
+          className="w-60 h-20 bg-element border border-element-lighter text-white rounded-xl hover:bg-element-light transition text-lg flex items-center justify-center"
         >
           Sign in with Riot
         </button>
-        <h2 className="text-4xl font-bold text-white mb-4">Working on Profiles</h2>
+        <h2 className="text-2xl text-white text-center max-w-3xl">By signing in with Riot you acknowledge that your profile becomes public.</h2>
         <button
           onClick={testLogin}
-          className="px-6 py-3 bg-red-600 text-white rounded-2xl shadow-md"
+          className="w-60 h-20 bg-element border border-element-lighter text-white rounded-xl hover:bg-element-light transition text-lg flex items-center justify-center"
         >
-          Test
+          Test (No Effect for Users)
         </button>
+        <div className="">
+          {!user ? (
+            <h2 className="text-2xl text-white text-center max-w-3xl">No user identified yet</h2>
+          ) : (
+            <h2 className="text-2xl text-white text-center max-w-3xl">Welcome, {user}!</h2>
+          )}
+        </div>
+
       </div>
      </div>
     );
   }
   
   export default Login;
+
+{/*
+.slice(0, 10)
+*/}
