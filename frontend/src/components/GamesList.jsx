9611549948a6e47ce452d605c304b1a91356349d
Emil Wallraff <emilwallraff@gmail.com>
2025-10-26 16:33:25 +0100

    added missing import

--- frontend/src/components/GamesList.jsx
+++ frontend/src/components/GamesList.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { ChevronDown, ChevronRight } from "lucide-react";
 
 import { agents } from "../data/hashcodes";
